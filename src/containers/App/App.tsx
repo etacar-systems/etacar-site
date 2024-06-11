b6ecb4b0cff4b893735a306fe8f31d001bf137bd
Tetrarion <mr.gilnich@gmail.com>
2024-06-11 15:37:01 +0300

    feat: add NavHeader

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import styles from "./App.module.scss";
+import { NavHeader } from "../../components/NavHeader";
 
 export const App = () => {
   return (
     <div className={styles.App}>
+      <NavHeader />
       <h1>HELLO ETACAR</h1>
     </div>
   );
