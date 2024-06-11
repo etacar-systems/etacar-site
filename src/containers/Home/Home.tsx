d06c229397681eeab807d6855bd794ca0df7118d
pavelchechelev <pashachechelev@gmail.com>
2024-06-11 20:57:12 +0300

    feat: add welcome section

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Mission } from './components/Mission';
-
+import React from "react";
+import { Mission } from "./components/Mission";
+import { Welcome } from "./components/Welcome";
 
 export const Home = () => {
   return (
     <div>
+      <Welcome />
       <Mission />
     </div>
   );
