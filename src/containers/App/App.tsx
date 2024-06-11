46cdb96170298db3b4a355e5db5f86dec5d51e1d
pavelrusetski <iampavelr@gmail.com>
2024-06-11 13:28:28 +0300

    feat: project setup

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./App.module.scss";
+
+export const App = () => {
+  return (
+    <div className={styles.App}>
+      <h1>HELLO ETACAR</h1>
+    </div>
+  );
+};
---
+++ src/routes/routesConfig.ts
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { App } from "../containers/App/App";
+
+export const routesConfig = [
+  // {
+  //   path: "/",
+  //   element: <App />,
+  // },
+  // {
+  //   path: "*",
+  //   element: <div></div>,
+  // },
+];
