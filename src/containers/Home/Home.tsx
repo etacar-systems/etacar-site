696e224f2ac215ad874748c534935f3bd86736f0
user <idonotwanttostudy@mail.ru>
2024-06-12 16:37:46 +0300

    added footer

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -6,6 +6,7 @@ import { Industries } from "./components/Industries";
 import { Mission } from "./components/Mission";
 import { Services } from "./components/Services";
 import { Welcome } from "./components/Welcome";
+import { Footer } from "../../components/Footer";
 
 export const Home = () => {
   return (
@@ -16,6 +17,7 @@ export const Home = () => {
       <CaseStudies />
       <Services />
       <Industries />
+      <Footer/>
     </div>
   );
 };
