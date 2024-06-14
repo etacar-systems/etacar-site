fe6400202da6debd2a0ae73b6b3951bb0fac3085
user <idonotwanttostudy@mail.ru>
2024-06-14 17:39:05 +0300

    feat: added models block on services page

---
+++ src/containers/ServicesPage/components/Models/data.ts
@@ -0,0 +1,40 @@
+import { ServicesImages } from "../../../../assets/images/Services";
+
+export const data = [
+  {
+    id: 1,
+    title: "Models of Co-operation: Custom Projects & Time-and-Materials",
+    description: "Engage a reliable and involved team with your project. Ourсaim is to develop long-term relationships as the go-to outsourcing partner in Belarus and smaller, fixed price tasks are a good way to start.",
+    advantages: [
+      "Track progress at all stages of development",
+      "You can easily vary scope of work during the development and implementation",
+      "Firm project management and on-time delivery",
+      "Services are payable by the hour",
+    ],
+    image: ServicesImages.ourTeam1,
+  },
+  {
+    id: 2,
+    title: "Dedicated Team Model",
+    description: "We build professional, full-time dedicated teams in Belarus. We thoroughly analyze all of your requirements and assemble the best IT specialists to work exclusively on your particular projects.",
+    advantages: [
+      "Our staff team is to work for you, as a part of your production, management, training and all other processes",
+      "Always motivated and well organized",
+      "Transparent budget and reporting",
+      "Competitive cost",
+    ],
+    image: ServicesImages.ourTeam2,
+  },
+  {
+    id: 3,
+    title: "IT Expert on Demand",
+    description: "We provide top-notch developers that will help you implement the best practices of product development. Our professionals have a long history working with Agile development methodology on numerous projects, and they will share their real world experience with your dedicated team.",
+    advantages: [
+      "Design product roadmap",
+      "Improve performance, stabilize architecture",
+      "Implement CI/CD",
+      "Conclude previous development team",
+    ],
+    image: ServicesImages.ourTeam3,
+  },
+]
