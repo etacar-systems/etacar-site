f9a052e7f70d19cd5f686dbc7676423471cae980
user <idonotwanttostudy@mail.ru>
2024-06-14 18:14:47 +0300

    feat: added case studies block

--- src/containers/CaseStudy/CaseStudy.tsx
+++ src/containers/CaseStudy/CaseStudy.tsx
@@ -5,11 +5,14 @@ import { Location } from '../../components/Location';
 import { caseStudiesGeographyData } from '../../model/caseStudies';
 import { Reviews } from '../../commonSections/Reviews';
 import { Footer } from '../../components/Footer';
+import { CaseStudies } from './components/CaseStudies';
+
 
 export const CaseStudy = () => {
   return (
     <div>
       <Welcome />
+      <CaseStudies />
       <Location sectionHeader='Clients Geography' geographyData={caseStudiesGeographyData}/>
       <Reviews textColor='dark' theme='dark'/>
       <Footer/>
