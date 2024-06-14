9a1b7b0143e887356ef19094149ecb8739d3abb9
user <idonotwanttostudy@mail.ru>
2024-06-14 17:54:46 +0300

    feat: added review block to about us and case studies pages

--- src/containers/AboutUs/AboutUs.tsx
+++ src/containers/AboutUs/AboutUs.tsx
@@ -2,8 +2,9 @@ import { Footer } from '../../components/Footer';
 import { Expirience } from './components/Experience';
 import { Introduction } from './components/Introduction';
 import { Location } from '../../components/Location';
-
+import { Reviews } from '../../commonSections/Reviews';
 import { aboutUsGeographyData } from './data';
+
 
 
 export const AboutUs = () => {
@@ -12,6 +13,7 @@ export const AboutUs = () => {
       <Introduction />
       <Expirience />
       <Location geographyData={aboutUsGeographyData}/>
+      <Reviews textColor={'dark'} theme={'dark'} />
       <Footer/>
     </>
   );
--- src/containers/CaseStudy/CaseStudy.tsx
+++ src/containers/CaseStudy/CaseStudy.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import { Welcome } from './components/Welcome';
 import { Location } from '../../components/Location';
 import { caseStudiesGeographyData } from '../../model/caseStudies';
+import { Reviews } from '../../commonSections/Reviews';
+import { Footer } from '../../components/Footer';
 
 export const CaseStudy = () => {
   return (
     <div>
       <Welcome />
       <Location sectionHeader='Clients Geography' geographyData={caseStudiesGeographyData}/>
+      <Reviews textColor='dark' theme='dark'/>
+      <Footer/>
     </div>
   );
 };
