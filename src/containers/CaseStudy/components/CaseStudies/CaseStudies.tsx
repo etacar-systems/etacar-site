2eb0c8e54ab1492e6e6e13980e8b84d17a57307c
user <idonotwanttostudy@mail.ru>
2024-06-17 15:20:20 +0300

    fix: case studies block

--- src/containers/CaseStudy/components/CaseStudies/CaseStudies.tsx
+++ src/containers/CaseStudy/components/CaseStudies/CaseStudies.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
 
-import { Section } from '../../../../components/Section';
 import { caseStudies } from '../../../../model/caseStudies';
 import { Item } from './components/Item';
 import style from './CaseStudies.module.scss';
 
+
 export const CaseStudies = () => {
   return (
-    <Section theme={'light'}>
-      <div className={style.swiper}>
-        <Swiper
-          direction={'horizontal'}
-          wrapperClass={style.swiper_wrapper}
-          modules={[Navigation]}
-          navigation={{ nextEl: '#swiper-forward', prevEl: '#swiper-back' }}
-          spaceBetween={10}
-        >
-          {caseStudies.map((item, index) => (
-            <SwiperSlide area-hidden key={index}>
-              <Item image={item.image} paragraphs={item.paragraphs} tags={item.tags} title={item.title} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </Section>
+    <div className={style.case_studies_container}>
+      {caseStudies.map((item, index) => 
+        index % 2 === 0
+        ? <Item index={index} orderReverse="false" image={item.image} paragraphs={item.paragraphs} tags={item.tags} title={item.title} />
+        : <Item index={index} orderReverse="true" image={item.image} paragraphs={item.paragraphs} tags={item.tags} title={item.title} />
+          )}
+    </div>
   );
 };
--- src/containers/CaseStudy/components/CaseStudies/components/Item/Item.tsx
+++ src/containers/CaseStudy/components/CaseStudies/components/Item/Item.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Button } from '../../../../../../components/Button';
 
 import { TagsContainer } from '../../../../../../components/TagsContainer';
-import { Button } from '../../../../../../components/Button';
-import { CASE_STUDY } from '../../../../../../routes';
-import { Navigation } from '../../../../../../components/Navigation';
 import style from './Item.module.scss';
 
 interface ItemProps {
@@ -12,29 +9,41 @@ interface ItemProps {
   paragraphs: string[];
   tags: string[];
   title: string;
+  orderReverse: "true" | "false";
+  index: number;
 }
 
-export const Item = ({ image, paragraphs, tags, title }: ItemProps) => {
-  const navigate = useNavigate();
+export const Item = ({ image, paragraphs, tags, title, orderReverse, index }: ItemProps) => {
 
   return (
-    <div className={style.container}>
+    <>
+    {index === 2
+    ? <div className={style.getStarted}>
+        <div className={style.textContainer}>
+          <div className={style.title}>Will your idea be next?</div>
+          <div>Get a free project estimation</div>
+        </div>
+        <div>
+          <Button type='solid' title='Get started' icon={true} onClick={() => {}}/>
+        </div>
+      </div>
+    : undefined}
+    <div order-reverse={orderReverse} className={style.container}>
       <div className={style.outer}>
-        <Navigation />
         <div className={style.inner}>
           <div className={style.title}>{title}</div>
-          <TagsContainer tags={tags} type={'light'} />
           <div>
             {paragraphs.map(paragraph => (
               <p>{paragraph}</p>
             ))}
           </div>
         </div>
-        <Button icon onClick={() => navigate(CASE_STUDY)} title={'All cases'} type={'ghost'} />
+        <TagsContainer tags={tags} type={'blue'} />
       </div>
       <div className={style.image}>
         <img src={image} alt={`${title}_icon`} />
       </div>
     </div>
+    </>
   );
 };
