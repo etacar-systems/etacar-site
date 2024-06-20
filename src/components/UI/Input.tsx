628f0609c0c310bf13e87cd41f15034b51a4e017
user <idonotwanttostudy@mail.ru>
2024-06-20 16:10:25 +0300

    feat: form submission without picture support

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames';
 import style from './Input.module.scss';
 
 interface InputProps {
@@ -7,15 +6,16 @@ interface InputProps {
   placeholder?: string;
   required?: boolean;
   theme?: 'light' | 'dark';
+  name?: string;
 }
 
-export const Input = ({ label, type, placeholder, theme = 'light' }: InputProps) => {
+export const Input = ({ label, type, required, placeholder, name, theme = 'light' }: InputProps) => {
   return (
     <div className={style.input_container}>
       <label data-theme={theme === 'dark' ? true : false} className={style.input_label}>
         {label}
       </label>
-      <input className={style.input} type={type} placeholder={placeholder} required />
+      <input name={name} className={style.input} type={type} placeholder={placeholder} required={required} />
     </div>
   );
 };
--- src/containers/ContactUs/components/ContactForm/ContactForm.tsx
+++ src/containers/ContactUs/components/ContactForm/ContactForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
 import colton from '../../../../assets/images/ContactUs/colton_bollinger.png';
 import { Input } from '../../../../components/UI/Input';
@@ -7,6 +7,18 @@ import { Button } from '../../../../components/Button';
 import linkArrow from '../../../../assets/icons/linkArrow.svg';
 
 export const ContactForm = () => {
+  const [state, handleSubmit] = useForm('xpwaazro');
+
+  if (state.succeeded) {
+    return (
+      <div className={style.container}>
+        <div className={style.contact_form_wrapper}>
+          <div className={style.success}>Thank you for your request</div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={style.container}>
       <div className={style.contact_form_wrapper}>
@@ -14,18 +26,29 @@ export const ContactForm = () => {
           <img src={colton} alt='Colton Bollinger' />
         </div>
         <div className={style.form_wrapper}>
-          <div className={style.form}>
+          <form onSubmit={handleSubmit} className={style.form}>
             <div className={style.form_row}>
-              <Input type='text' label='Full Name*' placeholder='John Smith' theme={'dark'} />
-              <Input type='text' label='Company Name' placeholder='IT Company' theme={'dark'} />
+              <Input name='fullName' type='text' label='Full Name*' placeholder='John Smith' theme={'dark'} required />
+              <ValidationError prefix='fullName' field='fullName' errors={state.errors} />
+              <Input
+                name='companyName'
+                type='text'
+                label='Company Name'
+                placeholder='IT Company'
+                theme={'dark'}
+                required={false}
+              />
+              <ValidationError prefix='companyName' field='companyName' errors={state.errors} />
             </div>
             <div className={style.form_row}>
-              <Input type='text' label='Phone number' placeholder='+1 (831) 333-6202' required theme={'dark'} />
-              <Input type='text' label='Email*' required placeholder='Example@gmail.com'  theme={'dark'} />
+              <Input name='phone' type='phone' label='Phone number' placeholder='+1 (831) 333-6202' theme={'dark'} />
+              <ValidationError prefix='phone' field='phone' errors={state.errors} />
+              <Input name='email' type='email' label='Email*' required placeholder='Example@gmail.com' theme={'dark'} />
+              <ValidationError prefix='email' field='email' errors={state.errors} />
             </div>
-            <textarea className={style.textarea} placeholder='Message'></textarea>
+            <textarea name='message' className={style.textarea} placeholder='Message'></textarea>
             <Button onClick={() => {}} title={'Send request'} type={'ghost'} />
-          </div>
+          </form>
           <div className={style.contacts}>
             <div className={style.left_section}>
               <div className={style.title}>Contacts</div>
@@ -50,15 +73,15 @@ export const ContactForm = () => {
                 <div className={style.info__text}>313 Pasadera Court, Monterey,</div>
                 <div className={style.info__text}>California 93940</div>
                 <div className={style.linkGoogleMap}>
-                <div className={style.googleMap}>
-                  <a target='_blank' href='https://maps.app.goo.gl/38kmmrR4fv51q8XE9'>
-                    Google map
-                  </a>
+                  <div className={style.googleMap}>
+                    <a target='_blank' href='https://maps.app.goo.gl/38kmmrR4fv51q8XE9'>
+                      Google map
+                    </a>
+                  </div>
+                  <div className={style.linkIcon}>
+                    <img className={style.linkArrow} src={linkArrow} alt='link arrow' />
+                  </div>
                 </div>
-                <div className={style.linkIcon}>
-                  <img className={style.linkArrow} src={linkArrow} alt='link arrow' />
-                </div>
-              </div>
               </div>
             </div>
           </div>
