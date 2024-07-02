2d31116673893fb2270f008ae9c237b86bcf48b8
Iliaz Mamedov <ilzmamedov@gmail.com>
2024-07-02 19:05:53 +0300

    fix: comments deleted

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,36 +1,3 @@
-// import React, { lazy, useEffect } from 'react';
-
-// // Alplabetical order is REQUIRED!
-
-// import { CaseStudies } from './components/CaseStudies';
-// import { Development } from './components/Development';
-// import { Footer } from '../../components/Footer';
-// import { GetEstimationForm } from './components/GetEstimationForm';
-// import { Industries } from '../../commonSections/Industries';
-// import { Mission } from './components/Mission';
-// import { Reviews } from '../../commonSections/Reviews';
-// import { Services } from './components/Services';
-// import { Welcome } from './components/Welcome';
-
-// export const Home = () => {
-//   useEffect(() => {
-//     window.scrollTo(0, 0);
-//   }, []);
-//   return (
-//     <div>
-//       <Welcome />
-//       <Mission />
-//       <Development />
-//       <CaseStudies />
-//       <Services />
-//       <Industries />
-//       <Reviews arrowColor='dark' theme='blueLight' />
-//       <GetEstimationForm />
-//       <Footer />
-//     </div>
-//   );
-// };
-
 import React, { lazy, Suspense, useEffect } from 'react';
 import Loader from '../../components/Loader';
 
