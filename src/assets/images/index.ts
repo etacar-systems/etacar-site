908bf1d2975b0783f0eeb9a9c03d4aacbcb1cc74
Iliaz Mamedov <ilzmamedov@gmail.com>
2024-07-08 16:32:49 +0300

    fix: unused imports deleted

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,93 +1,5 @@
-// import auctionHouseSoftware from './auctionHouseSoftware.webp';
-// import baseballStatisticManagement from './baseballStatisticManagement.webp';
-// import brandIdentityDesign from './brandIdentityDesign.webp';
-// import businessMarketplace from './businessMarketplace.svg';
-// import cloudMarketingSolution from './cloudMarketingSolution.webp';
-// import communityWebPlatform from './communityWebPlatform.webp';
-// import contentManagementSolution from './contentManagementSolution.webp';
-// import deliveryManagementSystem from './deliveryManagementSystem.svg';
-// import digitalProductPresentation from './digitalProductPresentation.webp';
-// import discountApplication from './discountApplication.webp';
-// import ecommerceB2BSolution from './ecommerceB2BSolution.webp';
-// import ecommerceB2CSolution from './ecommerceB2CSolution.webp';
-// import ecommerceMicroservices from './ecommerceMicroservices.webp';
-// import ecommerceSocialPlatform from './ecommerceSocialPlatform.webp';
-// import ecommerceSolutionBasedOnOCCPlatform from './ecommerceSolutionBasedOnOCCPlatform.webp';
-// import ecommerceSystemForHealthcare from './ecommerceSystemForHealthcare.webp';
-// import edtechPlatform from './edtechPlatform.webp';
-// import educationalPlatform from './educationalPlatform.webp';
-// import educationalSystem from './educationalSystem.webp';
-// import enterpriseProcurement from './enterpriseProcurement.svg';
-// import industrialSupplier from './industrialSupplier.webp';
-// import infrastructureMaintenance from './infrastructureMaintenance.webp';
-// import instagramInfluencerMarketingPlatform from './instagramInfluencerMarketingPlatform.webp';
-// import instagramMarketing from './instagramMarketing.webp';
-// import ITJobOnlineMarketplace from './ITJobOnlineMarketplace.webp';
-// import marketingPlatform from './marketingPlatform.webp';
-// import migrationFromLegacy from './migrationFromLegacy.webp';
-// import MLStartup from './MLStartup.webp';
-// import newGenCouponingSystem from './newGenCouponingSystem.webp';
-// import onDemandSupermarket from './onDemandSupermarket.svg';
-// import onlineJournal from './onlineJournal.webp';
-// import packagingDesign from './packagingDesign.webp';
-// import personalLearningPlan from './personalLearningPlan.svg';
-// import platformForInfluencers from './platformForInfluencers.webp';
-// import platformToSocialGifts from './platformToSocialGifts.webp';
-// import productStockManagement from './productStockManagement.svg';
-// import reitInvestment from './reitInvestment.webp';
-// import restarauntReservations from './restarauntReservations.webp';
-// import singaporeStartup from './singaporeStartup.webp';
-// import UXUIAndSMM from './UXUIAndSMM.webp';
-// import visualIdentityKit from './visualIdentityKit.webp';
-
-// export const images = {
-//   auctionHouseSoftware,
-//   baseballStatisticManagement,
-//   brandIdentityDesign,
-//   businessMarketplace,
-//   cloudMarketingSolution,
-//   communityWebPlatform,
-//   contentManagementSolution,
-//   deliveryManagementSystem,
-//   digitalProductPresentation,
-//   discountApplication,
-//   ecommerceB2BSolution,
-//   ecommerceB2CSolution,
-//   ecommerceMicroservices,
-//   ecommerceSocialPlatform,
-//   ecommerceSolutionBasedOnOCCPlatform,
-//   ecommerceSystemForHealthcare,
-//   edtechPlatform,
-//   educationalPlatform,
-//   educationalSystem,
-//   enterpriseProcurement,
-//   industrialSupplier,
-//   infrastructureMaintenance,
-//   instagramInfluencerMarketingPlatform,
-//   instagramMarketing,
-//   ITJobOnlineMarketplace,
-//   marketingPlatform,
-//   migrationFromLegacy,
-//   MLStartup,
-//   newGenCouponingSystem,
-//   onDemandSupermarket,
-//   onlineJournal,
-//   packagingDesign,
-//   personalLearningPlan,
-//   platformForInfluencers,
-//   platformToSocialGifts,
-//   productStockManagement,
-//   reitInvestment,
-//   restarauntReservations,
-//   singaporeStartup,
-//   UXUIAndSMM,
-//   visualIdentityKit,
-// };
-
-import appointmentScheduling from './appointmentScheduling.webp';
 import auctionHouseSoftware from './auctionHouseSoftware.webp';
 import baseballStatisticManagement from './baseballStatisticManagement.webp';
-import blockchainProject from './blockchainProject.webp';
 import brandIdentityDesign from './brandIdentityDesign.webp';
 import businessMarketplace from './businessMarketplace.svg';
 import cloudMarketingSolution from './cloudMarketingSolution.webp';
@@ -96,14 +8,12 @@ import contentManagementSolution from './contentManagementSolution.webp';
 import deliveryManagementSystem from './deliveryManagementSystem.svg';
 import digitalProductPresentation from './digitalProductPresentation.webp';
 import discountApplication from './discountApplication.webp';
-import dynamicManagementSystem from './dynamicManagementSystem.webp';
 import ecommerceB2BSolution from './ecommerceB2BSolution.webp';
 import ecommerceB2CSolution from './ecommerceB2CSolution.webp';
 import ecommerceMicroservices from './ecommerceMicroservices.webp';
 import ecommerceSocialPlatform from './ecommerceSocialPlatform.webp';
 import ecommerceSolutionBasedOnOCCPlatform from './ecommerceSolutionBasedOnOCCPlatform.webp';
 import ecommerceSystemForHealthcare from './ecommerceSystemForHealthcare.webp';
-import ecommerceWebApplication from './ecommerceWebApplication.webp';
 import edtechPlatform from './edtechPlatform.webp';
 import educationalPlatform from './educationalPlatform.webp';
 import educationalSystem from './educationalSystem.webp';
@@ -127,15 +37,12 @@ import reitInvestment from './reitInvestment.webp';
 import restarauntReservations from './restarauntReservations.webp';
 import saasInfluencerManagement from './saasInfluencerManagement.webp';
 import singaporeStartup from './singaporeStartup.webp';
-import socialFundraisingProduct from './socialFundraisingProduct.webp';
 import UXUIAndSMM from './UXUIAndSMM.webp';
 import visualIdentityKit from './visualIdentityKit.webp';
 
 export const images = {
-  appointmentScheduling,
   auctionHouseSoftware,
   baseballStatisticManagement,
-  blockchainProject,
   brandIdentityDesign,
   businessMarketplace,
   cloudMarketingSolution,
@@ -144,14 +51,12 @@ export const images = {
   deliveryManagementSystem,
   digitalProductPresentation,
   discountApplication,
-  dynamicManagementSystem,
   ecommerceB2BSolution,
   ecommerceB2CSolution,
   ecommerceMicroservices,
   ecommerceSocialPlatform,
   ecommerceSolutionBasedOnOCCPlatform,
   ecommerceSystemForHealthcare,
-  ecommerceWebApplication,
   edtechPlatform,
   educationalPlatform,
   educationalSystem,
@@ -175,7 +80,6 @@ export const images = {
   restarauntReservations,
   saasInfluencerManagement,
   singaporeStartup,
-  socialFundraisingProduct,
   UXUIAndSMM,
   visualIdentityKit,
 };
